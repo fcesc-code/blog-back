629d545100a276093d2d72e5b74ef6eb5eb493c5
fcesc-code <bpfran@gmail.com>
2021-10-02 18:25:26 +0200

    config / add config to access remote db and include secrets and keys in .gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,13 +7,16 @@ import { AuthModule } from './auth/auth.module';
 import { CategoriesModule } from './categories/categories.module';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
+import { typeOrmConfigAsync } from 'config/typeorm.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      keepConnectionAlive: true,
-      autoLoadEntities: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   keepConnectionAlive: true,
+    //   autoLoadEntities: true,
+    // }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     UsersModule,
     CategoriesModule,
     PostsModule,
